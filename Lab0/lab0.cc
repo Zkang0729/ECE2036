22f7b03515dc3bf28d514e1ecf6d6225a16fa133
zkang35 <harrydexiong@gmail.com>
2020-01-07 15:33:22 -0500

    Lab0

---
+++ Lab0/lab0.cc
@@ -0,0 +1,18 @@
+// I, Zixuan Kang, have successfully accessed my pace-ice account. I am
+// able to transfer files from my pace-ice account to my local machine using an
+// ftp client. I can use either vim or nano directly on pace-ice.
+// I am ready for the next assignment on pace-ice.
+
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+  std::cout << "This (\") is a double quote. "
+            << '\n'
+            << "This (\') is a single quote. "
+            << '\n'
+            << "This (\\) is a backslash. "
+            << '\n'
+            << "This (/) is a forward slash. ";
+}
